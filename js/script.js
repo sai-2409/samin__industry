4b13b1a253626a6c27d0230e1aaca1ebb2db3e2d
Садриддин Самиджонов <sai.laca@Sadriddins-Laptop.local>
2025-05-14 16:17:56 -0700

    searching the smooth property

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// Fixing the header to be sticky
+  const header = document.querySelector('.header');
+  const nav = document.querySelector('.nav');
+  const navHeight = nav.getBoundingClientRect().height;
+  
+  const stickyNav = function(entries) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+  };
+
+  const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+  });
+
+  headerObserver.observe(header);
+
+// Fixing the header to be sticky
+
+// Scrolling to sections smoothly
+const btnScrollTo1 = document.querySelector('.nav__link--1');
+
+
+
+// I should fix this code (slider for cards, functionalities, etc.)
 const cards = document.querySelectorAll('.card-card');
 let currentIndex = 1;
 
