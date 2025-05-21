acc56b613a01cd5113041aec9bdbe2aff73468f7
Садриддин Самиджонов <sai.laca@Sadriddins-Laptop.local>
2025-05-21 16:33:26 -0700

    added saved address function

--- js/saminCartScript.js
+++ js/saminCartScript.js
@@ -25,6 +25,53 @@ toggleForm(showCardBtn, cardInformation);
 toggleForm(addressFillForm, addressForm);
 
 
+// Bro, you know the filling when it seems that everything is going amazing, and you have a lot of stuff to do, at the same moment you wanna
+// chill and enjoy your life. I can not chill, not now ! 
+// Trying to save address when user clicks on save address button
+
+const form = document.querySelector('.address-form__form');
+const savedAddressList = document.getElementById('savedAddressList');
+let addressCount = 1;
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const fullName = document.getElementById('fullName').value;
+  const street = document.getElementById('streetAddress').value;
+  const city = document.getElementById('city').value;
+  const zipCode = document.getElementById('zipCode').value;
+
+  const cardHTML = `
+  
+  <div class="saved-address__card" width = "100%">
+        <div class="saved-address__number">${addressCount}</div>
+        <div class="saved-address__info">
+          <div class="saved-address__name">${fullName}</div>
+          <div class="saved-address__text">${street}</div>
+          <div class="saved-address__text">${city}, ${zipCode}</div>
+        </div>
+        <div class="saved-address__actions">
+          <button class="saved-address__button common__button">Изменить</button>
+          <button class="saved-address__button common__button">Удалить</button>
+        </div>
+      </div>
+  `
+  savedAddressList.insertAdjacentHTML('beforeend', cardHTML);
+  addressCount++;
+  form.reset();
+
+});
+
+
+
+
+
+
+
+
+
+
+
 // card.classList.add('hide__card');
 // setTimeout(() => card.remove(), 300);
 
